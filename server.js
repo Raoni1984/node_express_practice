1e40a1b978283156ee3ddda1db2cfcf1cc67a95b
Raoni Augusto Cesar Souza Cunha <raonico@gmail.com>
2022-02-04 15:37:32 -0500

    installing EJS

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'));
+  // response.sendFile(path.join(__dirname, './static/index.html'));
+  response.render('pages/index', { pageTitle: 'Bem-vindos' });
 });
 
 app.get('/speakers', (request, response) => {
